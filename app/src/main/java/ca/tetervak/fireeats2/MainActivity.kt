a941cd6eb1e8e67ac451c3efe7efab3d8fefe5ed
Alexander Tetervak <tetervak@sheridancollege.ca>
2021-11-23 11:18:15 -0500

    initial commit

---
+++ app/src/main/java/ca/tetervak/fireeats2/MainActivity.kt
@@ -0,0 +1,15 @@
+package ca.tetervak.fireeats2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        Navigation.findNavController(this, R.id.nav_host_fragment)
+            .setGraph(R.navigation.nav_graph)
+    }
+}
---
+++ app/src/main/java/ca/tetervak/fireeats2/MainActivityViewModel.kt
@@ -0,0 +1,13 @@
+package ca.tetervak.fireeats2
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * ViewModel for [com.google.firebase.example.fireeats.MainActivity].
+ */
+
+class MainActivityViewModel : ViewModel() {
+
+    var isSigningIn: Boolean = false
+    var filters: Filters = Filters.default
+}
